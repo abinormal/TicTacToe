c7ae61e3abe5fc22d336c728fc5416cf04c45f4c
Abi Sidford <asidford@gmail.com>
2021-10-12 22:10:28 +0100

    Finish testing for X to win Horizontally, Vertically and diagonally. Also check the move made is valid.

--- src/main/java/com/siddy/app/Board.java
+++ src/main/java/com/siddy/app/Board.java
@@ -11,6 +11,11 @@ public class Board {
     }
 
     public boolean makeMove(int x, int y, String player) {
+        // Check the move is valid
+        if (x > 2 || x < 0 || y > 2 || y < 0) {
+            // System.out.println("This move, it is not ready yet!");
+            return false;
+        }
         if (gameBoard[x][y].equals(" ")) {
             gameBoard[x][y] = player;
             return true;
@@ -18,22 +23,46 @@ public class Board {
         return false;
     }
 
-    public boolean hasWon(){
+    public boolean hasWon() {
+        // Horizontal
         int countXTop = 0;
         int countXMiddle = 0;
         int countXBottom = 0;
-        for (int i = 0 ; i < gameBoard.length; i++){
-            if (gameBoard[0][i].equals("X")){
+        for (int i = 0; i < gameBoard.length; i++) {
+            if (gameBoard[0][i].equals("X")) {
                 countXTop++;
             }
-            if (gameBoard[1][i].equals("X")){
+            if (gameBoard[1][i].equals("X")) {
                 countXMiddle++;
             }
-            if (gameBoard[2][i].equals("X")){
+            if (gameBoard[2][i].equals("X")) {
                 countXBottom++;
             }
-
         }
-        return countXTop == 3 || countXMiddle == 3 || countXBottom == 3;
+        // Vertical
+        int countXLeft = 0;
+        int countXCenter = 0;
+        int countXRight = 0;
+        for (String[] strings : gameBoard) {
+            if (strings[0].equals("X")) {
+                countXLeft++;
+            }
+            if (strings[1].equals("X")) {
+                countXCenter++;
+            }
+            if (strings[2].equals("X")) {
+                countXRight++;
+            }
+        }
+        // Diagonal
+        if (gameBoard[1][1].equals("X")) {
+            if (gameBoard[0][0].equals("X") && gameBoard[2][2].equals("X") ||
+                    gameBoard[0][2].equals("X") && gameBoard[2][0].equals("X")) {
+                return true;
+            }
+        }
+
+        return countXTop == 3 || countXMiddle == 3 || countXBottom == 3 ||
+                countXLeft == 3 || countXCenter == 3 || countXRight == 3;
     }
 }
--- src/test/java/com/siddy/app/BoardTest.java
+++ src/test/java/com/siddy/app/BoardTest.java
@@ -3,6 +3,8 @@ package com.siddy.app;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class BoardTest {
 
@@ -13,23 +15,45 @@ public class BoardTest {
         gameBoard = new Board();
     }
 
+    // Test the makeMove method
     @Test
     public void checkSpaceNotAlreadyTaken(){
-        assertEquals(true, gameBoard.makeMove(1,1,"X"));
+        assertTrue(gameBoard.makeMove(1,1,"X"));
     }
 
     @Test
     public void checkSpaceIsAlreadyTaken(){
         gameBoard.makeMove(0,1,"O");
-        assertEquals(false, gameBoard.makeMove(0,1,"X"));
+        assertFalse(gameBoard.makeMove(0,1,"X"));
     }
 
+    @Test
+    public void checkBadXMove(){
+        assertFalse(gameBoard.makeMove(3,0,"O"));
+    }
+
+    @Test
+    public void checkBadMinusXMove(){
+        assertFalse(gameBoard.makeMove(-1,2,"O"));
+    }
+
+    @Test
+    public void checkBadYMove(){
+        assertFalse(gameBoard.makeMove(2,4,"O"));
+    }
+
+    @Test
+    public void checkBadMinusYMove(){
+        assertFalse(gameBoard.makeMove(2,-2,"O"));
+    }
+
+    // Is there a winner?
     @Test
     public void checkWinnerIfRowTaken(){
         gameBoard.makeMove(0,0, "X");
         gameBoard.makeMove(0,1, "X");
         gameBoard.makeMove(0,2, "X");
-        assertEquals(true, gameBoard.hasWon());
+        assertTrue(gameBoard.hasWon());
     }
 
     @Test
@@ -37,7 +61,63 @@ public class BoardTest {
         gameBoard.makeMove(1,0,"X");
         gameBoard.makeMove(1,1,"X");
         gameBoard.makeMove(1,2,"X");
-        assertEquals(true, gameBoard.hasWon());
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkThirdRowTaken(){
+        gameBoard.makeMove(2,0,"X");
+        gameBoard.makeMove(2,1,"X");
+        gameBoard.makeMove(2,2,"X");
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkXNotWin(){
+        gameBoard.makeMove(2,0,"X");
+        gameBoard.makeMove(2,1,"X");
+        gameBoard.makeMove(2,2,"O");
+        assertFalse(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkLeftColumnXWin(){
+        gameBoard.makeMove(0,0,"X");
+        gameBoard.makeMove(1,0,"X");
+        gameBoard.makeMove(2,0,"X");
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkCenterColumnXWin(){
+        gameBoard.makeMove(0,1,"X");
+        gameBoard.makeMove(1,1,"X");
+        gameBoard.makeMove(2,1,"X");
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkRightColumnXWin(){
+        gameBoard.makeMove(0,2,"X");
+        gameBoard.makeMove(1,2,"X");
+        gameBoard.makeMove(2,2,"X");
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkBackSlashDiagonal(){
+        gameBoard.makeMove(0,0,"X");
+        gameBoard.makeMove(1,1,"X");
+        gameBoard.makeMove(2,2,"X");
+        assertTrue(gameBoard.hasWon());
+    }
+
+    @Test
+    public void checkForwardSlashDiagonal(){
+        gameBoard.makeMove(0,2,"X");
+        gameBoard.makeMove(1,1,"X");
+        gameBoard.makeMove(2,0,"X");
+        assertTrue(gameBoard.hasWon());
     }
 
 }
